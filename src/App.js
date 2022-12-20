14e3514f083c4dfed26f667facbae13d2766f282
Bhavika Mevada <109460560+bmevada@users.noreply.github.com>
2022-12-20 21:06:25 +1100

    debigging

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import Header from "../src/components/Header"
-import About from "./components/Resume";
-import Contact from "../src/components/Contact";
-import Portfolio from "../src/components/Portfolio";
-import Resume from "./components/About";
-import Footer from "../src/components/Footer";
+import Header from "../src/components/Header.js"
+import About from "./components/Resume.js";
+import Contact from "../src/components/Contact.js";
+import Portfolio from "../src/components/Portfolio.js";
+import Resume from "./components/About.js";
+import Footer from "../src/components/Footer.js";
 import { Helmet } from "react-helmet";
 import './App.css';
 
