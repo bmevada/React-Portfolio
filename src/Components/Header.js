349a5e05615a7358a01a7ad59563fe6e0ea8353d
Bhavika Mevada <109460560+bmevada@users.noreply.github.com>
2022-12-20 20:00:34 +1100

    add navigation bar content

---
+++ src/Components/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Nav from "../Nav";
+
+function Header(props) {
+  const { currentTab, handleTabChange } = props;
+
+  return (
+    <div>
+      <section>
+        <header className="flex-row px-1">
+          <div>
+            <h1>Bhavika Mevada's Portfolio</h1>
+          </div>
+          <div>
+            <Nav
+              currentTab={currentTab}
+              handleTabChange={handleTabChange}
+            ></Nav>
+          </div>
+        </header>
+      </section>
+      <section class="hero">
+        <div class="hero-cta">
+
+          <h2>Welcome to my Coding Bootcamp Portfolio</h2>
+          <p>
+            Add information -  Examples of my completed projects in Bootcamp can be found here
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Header;
