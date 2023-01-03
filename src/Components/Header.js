41f364a8070a89f8a529bd19de5b144dc72ae26b
Bhavika Mevada <109460560+bmevada@users.noreply.github.com>
2023-01-03 19:16:19 +1100

    page successfully loading

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,22 +1,22 @@
 import React from "react";
-import Nav from "../Navigation";
+// import Nav from "Navigation";
 
 function Header(props) {
-  const { currentTab, handleTabChange } = props;
+  // const { currentTab, handleTabChange } = props;
 
   return (
     <div>
       <section>
         <header className="flex-row px-1">
           <div>
-            <h1>Bhavika Mevada's Portfolio</h1>
+            <h1>Bhavika's Portfolio</h1>
           </div>
-          <div>
+          {/* <div>
             <Nav
               currentTab={currentTab}
               handleTabChange={handleTabChange}
             ></Nav>
-          </div>
+          </div> */}
         </header>
       </section>
       <section class="hero">
